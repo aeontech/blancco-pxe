0b96c3d598b8424967dc08a388c39a875ee58319
Shane Thompson <me@shane.pt>
2018-06-25 11:59:30 +0800

    Fixed RhStrapper yum variable error

--- src/RhStrapper.py
+++ src/RhStrapper.py
@@ -5,17 +5,17 @@ class RhStrapper(LinuxStrapper):
     packages = "".split(" ")
 
     def install_packages(self):
-        yum = yum.YumBase()
+        yb = yum.YumBase()
         yb.conf.cache = false
 
         try:
-            yum.doLock()
+            yb.doLock()
 
             # First off, we need epel-release
-            epel = yum.searchGenerator(['name'], ['epel-release'])
+            epel = yb.searchGenerator(['name'], ['epel-release'])
             print "%r" % epel
         finally:
-            yum.doUnlock()
+            yb.doUnlock()
 
     def configure_packages(self):
         pass
