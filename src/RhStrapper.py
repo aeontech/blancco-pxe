7d89d176d86a1fd1c322a163c06fd80fc63cec7b
Shane Thompson <me@shane.pt>
2018-06-25 12:37:57 +0800

    First successful yum install

--- src/RhStrapper.py
+++ src/RhStrapper.py
@@ -1,4 +1,5 @@
 import yum
+import log
 from LinuxStrapper import LinuxStrapper
 
 class RhStrapper(LinuxStrapper):
@@ -6,16 +7,31 @@ class RhStrapper(LinuxStrapper):
 
     def install_packages(self):
         yb = yum.YumBase()
-        yb.conf.cache = false
+        yb.conf.cache = False
 
         try:
             yb.doLock()
 
             # First off, we need epel-release
-            epel = yb.searchGenerator(['name'], ['epel-release'])
-            print "%r" % epel
+            self._mark_install(yb, 'epel-release')
+
+            yb.buildTransaction()
+            yb.processTransaction()
+
+            for package in self.packages:
+                self._mark_install(yb, package)
+
+            # yb.buildTransaction()
+            # yb.processTransaction()
         finally:
             yb.doUnlock()
 
     def configure_packages(self):
         pass
+
+    def _mark_install(self, yb, pkgname):
+        matching = yb.searchGenerator(['name'], [pkgname], False, True)
+        for (pkg, matched_value, matched_keys) in matching:
+            if matched_keys[0] == pkgname:
+                log.warn('Marking package "%s" for installation' % pkgname)
+                yb.install(pkg)
