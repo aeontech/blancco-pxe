2565865d945c5717162e9e79a78e14a13bb28bf6
Shane Thompson <me@shane.pt>
2018-07-11 10:58:36 +0800

    Stream reset didn't work... Maybe this will

--- stage.sh
+++ stage.sh
@@ -44,10 +44,6 @@ update () {
     fi
 }
 
-# Reset std streams
-# exec &>/dev/tty
-exec < /dev/tty
-
 update   # updates system
 install
 
@@ -56,7 +52,7 @@ cd ~
 git clone https://github.com/aeontechnology/blancco-pxe.git
 cd blancco-pxe
                     git checkout develop
-python stage.py
+python stage.py <&0 /dev/tty
 popd > /dev/null
 
 _info "Cleaning up..."
