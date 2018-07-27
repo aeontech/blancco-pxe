df45273c2378511f74c57f74acb0a4dcd0c76e20
Shane Thompson <me@shane.pt>
2018-07-27 08:01:30 +0800

    Add netplan config for ubuntu

--- src/Linux/Strapper/DebStrapper.py
+++ src/Linux/Strapper/DebStrapper.py
@@ -1,3 +1,4 @@
+import os
 import apt
 import sys
 from ... import log
@@ -29,3 +30,39 @@ class DebStrapper(LinuxStrapper):
         except Exception, arg:
             print >> sys.stderr, "Sorry, package installation failed: %s" % str(arg)
             raise RuntimeError("Package installation failed: %s" % str(arg)), None, sys.exc_info()[2]
+
+    def _configure_interfaces(self):
+        super(self.__class__, self)._configure_interfaces()
+
+        if os.path.isfile('/run/network/ifstate'):
+            self._configure_ifupdown()
+        else:
+            self._configure_netplan()
+
+    def _configure_ifupdown(self):
+        pass
+
+    def _configure_netplan(self):
+        file = '''
+        network:
+            version: 2
+            ethernets:
+                corp0:
+                    dhcp4: yes
+                pxe0:
+                    dhcp4: no
+                    dhcp6: no
+                    addresses: [%s/%d]
+                    nameservers:
+                        addresses: [%s]
+        ''' % (
+            self._pxe_int.getIpAddress(),
+            self._pxe_int.getCidrMask(),
+            self._pxe_int.getIpAddress()
+        )
+
+        f = open('/etc/netplan/01-netcfg.yaml', 'w')
+        f.write(file)
+        f.close()
+
+        os.system('netplan apply')
